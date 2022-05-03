56024d4461036df94db68ef8b9444943e7baedc6
Antoniak Loïc <loic.antoniak@gmail.com>
2022-05-03 15:07:29 +0200

    center the spinner

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -3,9 +3,11 @@ import "./Spinner.scss";
 
 export default function Spinner() {
   return (
-    <div className="spinner">
-      <div className="spinner_double-bounce1"></div>
-      <div className="spinner_double-bounce2"></div>
+    <div className="center">
+      <div className="spinner">
+        <div className="spinner_double-bounce1"></div>
+        <div className="spinner_double-bounce2"></div>
+      </div>
     </div>
   );
 }
